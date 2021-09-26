9496e915728acb292b0d6f92ffd6311a9b5cfef7
colesturza <colesturza@gmail.com>
2021-09-25 20:11:47 -0600

    Add assignment 3 dir.

---
+++ assignment_3/client/main.py
@@ -0,0 +1,21 @@
+import requests
+import time
+import os
+
+
+def main():
+
+    port = os.environ['PORT']
+    host = os.environ['HOST']
+    period = int(os.environ['PERIOD'])
+    repititions = int(os.environ['REPITITIONS'])
+
+    for _ in range(repititions):
+        response = requests.get(f'http://{host}:{port}/')
+        print(response.text)
+
+        time.sleep(period)
+
+
+if __name__ == '__main__':
+    main()
---
+++ assignment_3/server_v2/app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+msg = os.environ['ENDPOINT_MESSAGE']
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return msg
+
+
+if __name__ == '__main__':
+    app.run()
