b5c066bc03336abdb410636d6644979802f48f24
colesturza <colesturza@gmail.com>
2021-10-01 16:25:17 -0600

    Update

--- assignment_3/provisioning/confd/run_confd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run -v /home/vagrant/confd:/etc/confd \
-  -v /home/vagrant/confd/out:/tmp \
-  -it dockage/confd confd \
-  -onetime \
-  -backend etcd \
-  -node http://192.168.33.10:2379
---
+++ confd_test/server_v1/app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'A very interesting message.'
+
+
+if __name__ == '__main__':
+    app.run()
