ac490eefaa547388a574624dd67c0dedfe8f93f2
colesturza <colesturza@gmail.com>
2021-10-03 18:39:59 -0600

    update

--- confd_test/remove_exited_containers.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo docker rm $(docker ps --filter status=exited -q)
--- confd_test/run_registrator.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo docker run -d --rm \
-  --name=registrator \
-  --net=host \
-  --volume=/var/run/docker.sock:/tmp/docker.sock \
-  gliderlabs/registrator:latest \
-  --ip=192.168.33.10 \
-    etcd://192.168.33.10:2379
